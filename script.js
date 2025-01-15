7e065427eed6cd4ade7f4b5ea1a26391e46c9169
mrpsycho-dev <stinkmang6@gmail.com>
2025-01-15 15:35:46 +0500

    clear all funtion

--- script.js
+++ script.js
@@ -7,13 +7,14 @@ const input = document.querySelector('.input');
 let number = 16 ** 2;
 // console.log(number);
 
+const clearAll = function () {
+  grid.innerHTML = '';
+};
 const init = function () {
+  clearAll();
   for (let i = 0; i < number ** 2; i++) {
     let div = document.createElement('div');
-    grid.style.gridTemplateColumns = `repeat(${Math.sqrt(number)}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${Math.sqrt(number)}, 1fr)`;
     grid.appendChild(div);
-
     div.addEventListener('mouseover', function () {
       div.classList.add('hovered');
     });
@@ -23,5 +24,8 @@ init();
 button.addEventListener('click', function () {
   number = input.value ** 2;
   // console.log(number);
+  grid.style.gridTemplateColumns = `repeat(${Math.sqrt(number)}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${Math.sqrt(number)}, 1fr)`;
+  clearAll();
   init();
 });
