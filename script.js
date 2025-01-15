3bd6ee150df4a376f2be6769f055d37fd604fa45
mrpsycho-dev <stinkmang6@gmail.com>
2025-01-15 23:22:33 +0500

    added eraser black and random choice

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 'use strict';
 const grid = document.querySelector('.grid');
 const button = document.querySelector('.button');
+const black = document.querySelector('.black');
+const random = document.querySelector('.random');
+const eraser = document.querySelector('.eraser');
+
 const input = document.querySelector('.input');
 let number = 16;
+let color = 'random';
+black.addEventListener('click', function () {
+  color = 'black';
+});
+random.addEventListener('click', function () {
+  color = 'random';
+});
+eraser.addEventListener('click', function () {
+  color = 'white';
+});
 
 const getRandomRGBColor = function () {
   const r = Math.trunc(Math.random() * 256);
@@ -19,8 +33,13 @@ const init = function () {
     grid.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${number}, 1fr)`;
     div.addEventListener('mouseover', function () {
-      // div.classList.add('hovered');
-      div.style.backgroundColor = getRandomRGBColor();
+      if (color === 'random') {
+        div.style.backgroundColor = getRandomRGBColor();
+      } else if (color === 'black') {
+        div.style.backgroundColor = '#000';
+      } else if (color === 'white') {
+        div.style.backgroundColor = '#fff';
+      }
     });
   }
 };
