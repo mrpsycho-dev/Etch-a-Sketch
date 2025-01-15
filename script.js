13256eb5601b707bed7731e22e41d05b1dbe68fd
mrpsycho-dev <stinkmang6@gmail.com>
2025-01-15 22:32:09 +0500

    random colors now generate and more visual changes

--- script.js
+++ script.js
@@ -4,18 +4,23 @@ const button = document.querySelector('.button');
 const input = document.querySelector('.input');
 let number = 16;
 
-const clearAll = function () {
-  grid.innerHTML = '';
+const getRandomRGBColor = function () {
+  const r = Math.trunc(Math.random() * 256);
+  const g = Math.trunc(Math.random() * 256);
+  const b = Math.trunc(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
 };
+
 const init = function () {
-  clearAll();
+  grid.innerHTML = '';
   for (let i = 0; i < number ** 2; i++) {
     let div = document.createElement('div');
     grid.appendChild(div);
     grid.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${number}, 1fr)`;
     div.addEventListener('mouseover', function () {
-      div.classList.add('hovered');
+      // div.classList.add('hovered');
+      div.style.backgroundColor = getRandomRGBColor();
     });
   }
 };
